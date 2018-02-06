070bd06f45f8a824e9102c3e12749a1621dc3782
rinshasaheer <33858066+rinshasaheer@users.noreply.github.com>
2018-02-06 11:38:57 +0530

    Update company.js

--- routes/company.js
+++ routes/company.js
@@ -13,54 +13,101 @@ var http = require('http');
 // Returns       : 
 // Author        : Jooshifa
 // Date          : 2-01-2018
-// Last Modified : 2-01-2018, Jooshifa Desc
+// Last Modified : 31-01-2018, Rinsha 
 // Desc          : to change status of company when its expired
 var CronJob = require('cron').CronJob;
-new CronJob('*/1 * * * * *', function() {
-  console.log('You will see this message every second');
+new CronJob('*/1 * * * * *', function () {
+    console.log('You will see this message every second');
 
 
-    Company.find({ $or:[ {cmp_status : 'Not Verified'},{cmp_status : 'Trail'} ]}, function(err, TrialCompanys) {
-        
-        TrialCompanys.forEach(function(trailComp) {
-            var expiredDate = new Date( trailComp.reg_datetime); 
-            expiredDate.setDate( expiredDate.getDate() +  config.trail_period );
-                if(expiredDate <= Date.now()){
-                    console.log(trailComp.id)
+    // Company.find({ $or:[ {cmp_status : 'Not Verified'},{cmp_status : 'Trail'} ]}, function(err, TrialCompanys) {
+
+    //     TrialCompanys.forEach(function(trailComp) {
+    //         var expiredDate = new Date( trailComp.reg_datetime); 
+    //         expiredDate.setDate( expiredDate.getDate() +  config.trail_period );
+    //             if(expiredDate <= Date.now()){
+    //                     var extServerOptions = {
+    //                         host: 'localhost',
+    //                         port: '3000',
+    //                         path: '/company/expiredsocket/'+trailComp.id,
+    //                         method: 'GET',
+
+    //                     };
+
+    //                     function get() {
+    //                         http.request(extServerOptions, function (res) {
+    //                             res.setEncoding('utf8');
+
+
+    //                         }).end();
+    //                     };
+
+    //                     get();
+
+    //                     Company.findOneAndUpdate({_id : trailComp.id }, 
+    //                             { $set: { cmp_status: "Expired" }}, 
+    //                             { new: true }, 
+    //                             function(err, doc) {
+    //                                     if(err){
+    //                                         console.log("Error in editing")
+    //                                     }
+    //                                     else{
+    //                                         console.log("Success")
+    //                                     }
+
+    //                             });
+
+    //             }
+    //     })
+    // });
+    Company.find({ cmp_status: { $ne: 'Expired' } }, function (err, notExpiredComp) {
+        if (err) {
+            throw err;
+        }
+        if (notExpiredComp) {
+            // console.log(notExpiredComp);
+            notExpiredComp.forEach(eachComp => {
+                currentPlan = eachComp.plans[eachComp.plans.length - 1];
+                // console.log("current plan of " + eachComp.organization + " is" + currentPlan);
+                if (currentPlan) {
+                    totalDays = currentPlan.no_month * 30;
+                    // console.log(" total days of " +eachComp.organization+ " is " +totalDays);
+                    dateofExpire = currentPlan.upgraded_date_time.setDate(currentPlan.upgraded_date_time.getDate() + totalDays);
+                     console.log(" date of expire of " +eachComp.organization+ " is " +dateofExpire);
+                    if (dateofExpire <= Date.now()) {
                         var extServerOptions = {
                             host: 'localhost',
                             port: '3000',
-                            path: '/company/expiredsocket/'+trailComp.id,
+                            path: '/company/expiredsocket/' + eachComp._id,
                             method: 'GET',
-                            
+
                         };
-                       
                         function get() {
                             http.request(extServerOptions, function (res) {
                                 res.setEncoding('utf8');
 
-                        
+
                             }).end();
                         };
-                        
                         get();
-    
-                        Company.findOneAndUpdate({_id : trailComp.id }, 
-                                { $set: { cmp_status: "Expired" }}, 
-                                { new: true }, 
-                                function(err, doc) {
-                                        if(err){
-                                            console.log("Error in editing")
-                                        }
-                                        else{
-                                            console.log("Success")
-                                        }
-                                
-                                });
-                        
+                        Company.findOneAndUpdate({ _id: eachComp._id },
+                            { $set: { cmp_status: "Expired" } },
+                            { new: true },
+                            function (err, doc) {
+                                if (err) {
+                                    console.log("Error");
+                                }
+                                else {
+                                    console.log("Success")
+                                }
+
+                            }
+                        );
+                    }
                 }
-        })
+            });
+        }
     });
-      }, null, true, 'America/Los_Angeles');
+}, null, true, 'America/Los_Angeles');
 // ----------------------------------End-------------------------------------------
 module.exports = router;
